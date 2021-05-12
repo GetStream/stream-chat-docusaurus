a9f36236f77dac2f1e5adbf7a05eaf86b967fa82
Vir Desai <virdesai@virs-mbp-2.lan>
2021-05-12 14:24:50 -0400

    testing build

--- scripts/s3-rewrite-html.js
+++ scripts/s3-rewrite-html.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const bucket = process.env.AWS_S3_BUCKET;
+const bucket = 'stream-website-staging/chat/docs/sdk';
 const build_path = process.env.DOCUSAURUS_BUILD_PATH;
 
 // Recursively read all html files generate by docusaurus and generate a S3 friendly cp command for them without .html extension
