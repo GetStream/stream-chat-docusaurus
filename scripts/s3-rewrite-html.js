932e0115ccad09e7817d8eb5e7acd48d26aaa801
Vir Desai <virdesai@virs-mbp-2.lan>
2021-05-12 12:44:21 -0400

    testing build

--- scripts/s3-rewrite-html.js
+++ scripts/s3-rewrite-html.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const bucket = process.env.AWS_STAGING_S3_BUCKET;
+const bucket = process.env.AWS_S3_BUCKET;
 
 // Recursively read all html files generate by docusaurus and generate a S3 friendly cp command for them without .html extension
 recursive('./', ['page-data', 'static', 'images', '.DS_Store', '*.js', '*.map', '*.css'])
