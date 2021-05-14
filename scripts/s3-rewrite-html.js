ac8306e95ba752ff452fffa523b4ac682103fc94
Vir Desai <virdesai@virs-mbp-2.lan>
2021-05-14 16:44:41 -0400

    testing build

--- scripts/s3-rewrite-html.js
+++ scripts/s3-rewrite-html.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const chat_docs_bucket = process.env.AWS_S3_BUCKET.replace('s3://', '').replace('/sdk/', '');
+const bucket = process.env.AWS_S3_BUCKET.replace('s3://', '').slice(0, -1);
 const build_path = process.env.DOCUSAURUS_BUILD_PATH;
 
 // Recursively read all html files generate by docusaurus and generate a S3 friendly cp command for them without .html extension
@@ -19,12 +19,10 @@ recursive(build_path, ['assets', 'img', 'images', '.DS_Store', '*.txt', '*.js', 
         const file = file_path.replace(build_path, '');
         let path = file.replace('.html', '');
         if (path.endsWith('index')) path = path.slice(0, -5);
-        if (!path.endsWith('/')) path = `${path}/`;
-
-        const bucket = `${chat_docs_bucket}${path === '/' ? '' : '/sdk'}`;
+        if (!path.endsWith('/')) path = `${path === '' ? 'sdk' : path}/`;
 
         s3.copyObject({
-          Bucket: bucket,
+          Bucket: path === 'sdk/' ? bucket.replace('/sdk', '') : bucket,
           CopySource: `/${bucket}/${file}`,
           Key: path,
           ContentType: 'text/html',
